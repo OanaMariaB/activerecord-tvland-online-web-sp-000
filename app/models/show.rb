16fadc4c860b0e2da6ff2f2d14d326cfc61af075
Oana Berentan <oana.berentan@yahoo.com>
2019-10-06 14:50:25 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network
 
   def actors_list
+    self.characters.collect do |character|
+      "#{charter.actor.first_name} #{charter.actor.last_name}"
   end
 end
